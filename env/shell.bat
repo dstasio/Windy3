78ff7cf2afdde744548fa6007eb6ca36db308ffc
Davide Stasio <doctegl@gmail.com>
2022-02-26 14:19:57 +0100

    removed vimconfig submodule

--- env/shell.bat
+++ env/shell.bat
@@ -1,5 +1,21 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-set path=X:\windy3\env;X:\lib;%path%
+set vcvars_path="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+IF NOT EXIST %vcvars_path% (
+	set vcvars_path="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+)
+call %vcvars_path%
+
 set DIRCMD=/o
-set HOME=X:\windy3\env\vimconfig
+
+set proj_root=%~dp0
+pushd "%proj_root%\.."
+set proj_root=%cd%
+popd
+
+set path="%proj_root%\env";%path%
+
+IF NOT EXIST "%proj_root%\env\vimconfig" (
+    set HOME=X:\global_vim
+) ELSE (
+    set HOME=%proj_root%\env\vimconfig
+)
